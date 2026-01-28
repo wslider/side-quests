0a1b58c58d22c2ae0d431a72c0af88ae0074039b
wslider <wslider2000@gmail.com>
2026-01-28 13:18:45 -0500

    Add logo.svg asset and update AppHeader.vue to include logo

--- sideQuests/src/components/AppHeader.vue
+++ sideQuests/src/components/AppHeader.vue
@@ -2,7 +2,20 @@
 </script>
 
 <template>
+
+    <h1>Side Quests</h1>
+
     <header class="app-header">
-        <h1>Side Quests</h1>
+        <div class="logo-container">
+        <img 
+            src="@/assets/logo.svg" 
+            alt="Side Quests Portal Logo" 
+            class="logo"
+            id="side-quests-logo"
+            width="120" 
+            height="120"
+        />
+</div>
+        
     </header>
 </template>
