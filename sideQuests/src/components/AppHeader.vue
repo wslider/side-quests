43f8b9422a8ce35a774ea763303a2f94e34e8003
wslider <wslider2000@gmail.com>
2026-01-28 13:31:58 -0500

    Add side-quests logo and update AppHeader.vue to use new image

--- sideQuests/src/components/AppHeader.vue
+++ sideQuests/src/components/AppHeader.vue
@@ -3,19 +3,32 @@
 
 <template>
 
-    <h1>Side Quests</h1>
-
     <header class="app-header">
+        <h1>Side Quests</h1>
         <div class="logo-container">
         <img 
-            src="@/assets/logo.svg" 
+            src="@/assets/side-quests-logo.png" 
             alt="Side Quests Portal Logo" 
             class="logo"
             id="side-quests-logo"
-            width="120" 
-            height="120"
+            width="160" 
+            height="160"
         />
 </div>
         
     </header>
+    
 </template>
+
+<style scoped>
+
+.app-header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    justify-content: space-evenly;
+    margin: 1rem 0;
+}
+
+</style>
